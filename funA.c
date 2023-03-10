24b519ddc5eaacd0b24968efc829ccc8212403ca
Jack Liao <xyz1357997531@gmail.com>
2023-03-10 17:43:34 +0800

    add funA

--- funA.c
+++ funA.c
@@ -1,14 +1,19 @@
 #include<stdio.h>
 #include<stdio.h>
 #include<math.h>
+
+float set_account(float dollar){
+	printf("==============================\n");
+	printf("Set Account money to %.2f$\n",dollar);
+	printf("==============================\n");
+	return dollar;
+}
 int main(){
-	
-	for(;;)
-		printf("loop");
+	float price;
+	printf("Set Account money:");
+	scanf("%f",&price);	
+	price = set_account(price);
 	printf("\n");		
-	
-	
-	
-	
 return 0;
 }
+
--- funB.c
+++ funB.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 #include<stdio.h>
 #include<math.h>
-int main(){
-	
-	for(;;)
-		printf("loop");
-	printf("\n");		
-	
-	
-	
+
+float sampoo(float account,float price){
+	printf("-------sampoo-------");
+	printf("\nsampoo's price is %.2f$\n",price);
+	printf("Account still remain %.2f\n",(account-price));
+	return account;
+}
+int main(){		
+//sampoo(,);	
 	
 return 0;
 }
